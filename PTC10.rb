00a4ad2f8ab60bed2c7b641aa98d0742efcd9b27
itgsod-felix-lennartsson <felix.lennartsson@elev.it-gymnasiet.se>
2014-11-05 14:38:48 +0100

    PTC 1.2
    Made it good

--- PTC10.rb
+++ PTC10.rb
@@ -1,16 +1,38 @@
+## PTC 1.2 ##
+
+# Variables:
+
 c = 0
 
-puts "Pythagorium Theorem calculator"
-puts "By Felix Lennartsson"
-puts
-puts "Please enter the first catheder (a):"
-a = gets.chomp.to_f
-puts "Please enter the seconds catheder (b):"
-b = gets.chomp.to_f
+# Functions:
 
-c = (a**2 + b**2)**0.5
+def getIn
+  puts "Pythagorium Theorem calculator"
+  puts "By Felix Lennartsson"
+  puts
+  puts "Please enter the first catheder (a):"
+  a = gets.chomp.to_f
+  puts "Please enter the seconds catheder (b):"
+  b = gets.chomp.to_f
+  return a,b
+end
 
-puts "The hypothenusa is #{c}"
+def calcIn(a,b)
+  c = (a**2 + b**2)**0.5
+  return c
+end
+
+def doOut(c)
+  puts "The hypothenuse is #{c}."
+end
+
+# Runcode:
+
+getIn
+calcIn(a,b)
+doOut(c)
+
+
 
 
 
