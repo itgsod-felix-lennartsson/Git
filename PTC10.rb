ac4cadd538f12244fc4934e84bd8796609af710c
itgsod-felix-lennartsson <felix.lennartsson@elev.it-gymnasiet.se>
2014-11-05 14:51:08 +0100

    PTC1.3
    Fixed bug with variables, now using global variables as a workaround.

--- PTC10.rb
+++ PTC10.rb
@@ -11,15 +11,14 @@ def getIn
   puts "By Felix Lennartsson"
   puts
   puts "Please enter the first catheder (a):"
-  a = gets.chomp.to_f
+  $a = gets.chomp.to_f
   puts "Please enter the seconds catheder (b):"
-  b = gets.chomp.to_f
-  return a,b
+  $b = gets.chomp.to_f
 end
 
-def calcIn(a,b)
-  c = (a**2 + b**2)**0.5
-  return c
+def calcIn
+  c = ($a**2 + $b**2)**0.5
+return c
 end
 
 def doOut(c)
@@ -29,7 +28,7 @@ end
 # Runcode:
 
 getIn
-calcIn(a,b)
+calcIn
 doOut(c)
 
 
